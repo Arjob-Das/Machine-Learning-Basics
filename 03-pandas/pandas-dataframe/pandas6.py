fedb81cbc8dc9f5d11ccd7794cee9e8fdbfa6054
Arjob <titoflagon@gmail.com>
2023-08-12 21:41:38 +0530

    Started Groupby using pandas

--- 03-pandas/pandas-dataframe/pandas6.py
+++ 03-pandas/pandas-dataframe/pandas6.py
@@ -3,3 +3,18 @@ import pandas as pd
 
 from numpy.random import randn
 
+#groupby using pandas
+
+data = {'Company':['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+       'Person':['Sam','Charlie','Amy','Vanessa','Carl','Sarah'],
+       'Sales':[200,120,340,124,243,350]}
+
+df=pd.DataFrame(data)
+
+print("Initial Dataframe : \n",df)
+
+bycomp=df.groupby('Company')
+print("Mean of Sales after Grouping by Company Column : \n",bycomp.mean())  #person column cannot be used for mean as it contains strings
+
+
+
