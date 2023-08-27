bf65494dd4e949b2a61db84f3c0de12405a2707e
Arjob <titoflagon@gmail.com>
2023-08-27 23:33:54 +0530

    Update seaborn03.py

--- 05-Seaborn/seaborn03.py
+++ 05-Seaborn/seaborn03.py
@@ -3,5 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.ion()
 tips = sns.load_dataset('tips')
+flights = sns.load_dataset('flights')
 print("Entire tips dataset : \n{d}".format(d=tips))
 print("Head of tips dataset : \n{d}".format(d=tips.head()))
+print("Entire flights dataset : \n{d}".format(d=flights))
+print("Head of flights dataset : \n{d}".format(d=flights.head()))
