35fc7619b3b841ddc8d8c28b13b9a01ab2c1a7ee
Arjob <titoflagon@gmail.com>
2023-09-17 08:07:51 +0530

    Update lr01.py

--- 08-Linear Regression/lr01.py
+++ 08-Linear Regression/lr01.py
@@ -5,3 +5,10 @@ import matplotlib
 import seaborn
 import sklearn as sk
 from sklearn.model_selection import train_test_split
+""" 
+from sklearn.cross_validation import train_test_split
+
+has been changed to :
+
+from sklearn.model_selection import train_test_split
+"""
