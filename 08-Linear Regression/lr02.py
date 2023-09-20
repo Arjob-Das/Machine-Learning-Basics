bf9fad9d28570ff802750ad1207407c6b63ae617
Arjob <titoflagon@gmail.com>
2023-09-20 17:30:24 +0530

    Update lr02.py

---
+++ 08-Linear Regression/lr02.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import sklearn as sk
+from sklearn.model_selection import train_test_split
+plt.ion()
+
+X = df[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+        'Avg. Area Number of Bedrooms', 'Area Population']]
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=101)
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+print("Intercept of the model : \n", lm.intercept_)
+print("Coefficients of the model : \n", lm.coef_)
+print("Columns of X_train : \n", X_train.columns)
+print("Score of the model : \n", lm.score(X_test, y_test))
+cdf = pd.DataFrame(lm.coef_, X.columns, columns=['Coeff'])
+print("Coefficient Dataframe : {d}\n".format(d=cdf))
+print("Head of Coefficient Dataframe : \n{d}".format(d=cdf.head()))
+predictions = lm.predict(X_test)
+print("Array of predictions : \n", predictions)
+
