5dae2c2e6e8a0c420f2a417a7cc1826cbe70a1ca
Arjob <titoflagon@gmail.com>
2023-09-28 05:08:48 +0530

    Update LR03.py

--- 09-Logistic Regression/LR03.py
+++ 09-Logistic Regression/LR03.py
@@ -11,6 +11,9 @@ from plotly import __version__
 import pandas as pd
 import numpy as np
 import chart_studio.plotly as py
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, classification_report, confusion_matrix
 plt.ion()
 
 train = pd.read_csv('titanic_train.csv')
@@ -44,6 +47,17 @@ train.drop(['Sex', 'Embarked', 'Name', 'Ticket',
            'PassengerId'], axis=1, inplace=True)
 print("Head of dataset : \n{d}".format(d=train.head()))
 
+# splitting train data into test and train data set
+# could have been done by importing and trimming the test csv file but this shows the process where the data isn't split into test and train
+X = train.drop('Survived', axis=1)
+y = train['Survived']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=101)
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+predictions = logmodel.predict(X_test)
 
+print("Classification Report : \n", classification_report(y_test, predictions))
+print("Confusion Matrix : \n", confusion_matrix(y_test, predictions))
 
 plt.waitforbuttonpress()
