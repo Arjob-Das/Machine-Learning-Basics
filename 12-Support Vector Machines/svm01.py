884c49118ad63ba9438c5da903fc575937ff0df0
DarkSoulHunter <titoflagon@gmail.com>
2023-12-14 11:59:12 +0530

    Update svm01.py

---
+++ 12-Support Vector Machines/svm01.py
@@ -0,0 +1,49 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+plt.ion()
+
+cancer = load_breast_cancer()
+print("cancer.keys(): \n{d}".format(d=cancer.keys()))
+print("Description of the dataset : \n{d}".format(d=cancer['DESCR']))
+df_feat = pd.DataFrame(cancer['data'], columns=cancer['feature_names'])
+print("Head of df_feat dataset : \n{d}".format(d=df_feat.head()))
+print("Target names of cancer dataset : \n{d}".format(
+    d=cancer['target_names']))
+df_target = pd.DataFrame(cancer['target'], columns=['Cancer'])
+print("Head of df_target dataset : \n{d}".format(d=df_target.head()))
+
+X = df_feat
+Y = df_target
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=101)
+model = SVC()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print("1st Classification Report : \n{d}".format(
+    d=classification_report(y_test, predictions)))
+print("1st Confusion Matrix : \n{d}".format(
+    d=confusion_matrix(y_test, predictions)))
+
+X = df_feat
+Y = np.ravel(df_target) #to remove column 1d array warning
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=101)
+model = SVC()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print("2nd Classification Report : \n{d}".format(
+    d=classification_report(y_test, predictions)))
+print("2nd Confusion Matrix : \n{d}".format(
+    d=confusion_matrix(y_test, predictions)))
+
+#from sklearn.grid_search import GridSearchCV -> depriciated
+from sklearn.model_selection import GridSearchCV
+
