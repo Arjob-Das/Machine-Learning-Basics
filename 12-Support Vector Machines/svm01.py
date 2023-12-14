bf5228cc9fd69d064d7e6a81cc9389adeeb045b9
DarkSoulHunter <titoflagon@gmail.com>
2023-12-14 12:07:16 +0530

    Update svm01.py

--- 12-Support Vector Machines/svm01.py
+++ 12-Support Vector Machines/svm01.py
@@ -33,7 +33,7 @@ print("1st Confusion Matrix : \n{d}".format(
     d=confusion_matrix(y_test, predictions)))
 
 X = df_feat
-Y = np.ravel(df_target) #to remove column 1d array warning
+Y = np.ravel(df_target)  # to remove column 1d array warning
 X_train, X_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.3, random_state=101)
 model = SVC()
@@ -44,6 +44,16 @@ print("2nd Classification Report : \n{d}".format(
 print("2nd Confusion Matrix : \n{d}".format(
     d=confusion_matrix(y_test, predictions)))
 
-#from sklearn.grid_search import GridSearchCV -> depriciated
-from sklearn.model_selection import GridSearchCV
+# from sklearn.grid_search import GridSearchCV -> depriciated
 
+param_grid = {'C': [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+              'gamma': [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001], 'kernel': ['rbf']}
+grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=3)
+grid.fit(X_train, y_train)
+print("Best Parameters : \n{d}".format(d=grid.best_params_))
+print("Best Estimator : \n{d}".format(d=grid.best_estimator_))
+grid_predictions = grid.predict(X_test)
+print("3rd Classification Report : \n{d}".format(d=classification_report(
+    y_test, grid_predictions)))
+print("3rd Confusion Matrix : \n{d}".format(d=confusion_matrix(
+    y_test, grid_predictions)))
